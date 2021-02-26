d1ff9404a3039fde7a5dc09ddf2da637dc00fc93
Jengyoung <young_pallete@naver.com>
2021-02-26 23:54:54 +0900

    숫자 야구게임 구현

--- 4강/numberBaseballGame.js
+++ 4강/numberBaseballGame.js
@@ -24,11 +24,8 @@ function checkIfCorrect(numbers) {
         for (let number of numbers) {
             const checkIfValidNum = answer.indexOf(number);
             if (checkIfValidNum < 0) continue;
-            if (numbers.indexOf(number) === checkIfValidNum) {
-                strike += 1;
-            } else {
-                ball += 1;
-            }
+            if (numbers.indexOf(number) === checkIfValidNum) strike ++;
+            else ball++;
         }
         const hint = document.createElement('li');
         hint.innerText = `${cnt}번째 시도: ${strike}S ${ball}B`;
@@ -38,28 +35,24 @@ function checkIfCorrect(numbers) {
 
 function getRandom(answer) {
     let randomNum = (parseInt(Math.random()*10)).toString();
-    while (answer.indexOf(randomNum) >= 0) {
-        randomNum = (parseInt(Math.random()*10)).toString();
-    }
+    while (answer.indexOf(randomNum) >= 0) randomNum = (parseInt(Math.random()*10)).toString();
     return randomNum;
 }
 
+/************************* eventListenr ***************************/
 startBtn.addEventListener('click', () => {
     initialize();
     for (let i=0; i<4; i++) answer += getRandom(answer);
-    console.log(answer);
 })
 
 inputBtn.addEventListener('click', () => {
     if (!answer) {
-        alert("시작 버튼을 눌러주세요")
+        alert("시작 버튼을 눌러주세요");
         return;
     };
+
     //check if number (4 digits)
     const val = input.value;
-    if (Number(parseInt(val)) && val.length === 4) {
-        checkIfCorrect(val);
-    } else {
-        alert("제대로 된 값을 눌러주세요.")
-    }
+    if (Number(parseInt(val)) && val.length === 4) checkIfCorrect(val);
+    else alert("제대로 된 값을 입력해주세요.");
 })
